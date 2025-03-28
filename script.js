8edc3f3e56b1fce14996a843c4639f690b3af4cc
Alex Milkovskyi <a.milkovsky@gmail.com>
2025-03-28 21:44:51 +0100

    Home page.

--- script.js
+++ script.js
@@ -1,73 +1,86 @@
 (function () {
   document.addEventListener("DOMContentLoaded", function() {
     const params = new URLSearchParams(window.location.search);
-    if (!params.has('song-uri')) {
-      document.getElementById('message').textContent = 'No song-uri was found.';
-      return;
-    }
+    let player;
 
-    function convertSpotifyUrlToUri(url) {
-      return url.replace("https://open.spotify.com/", "spotify:").replace(/track\//, 'track:');
-    }
+    if (params.has('song-uri')) {
+      document.getElementById('message').textContent = 'Loading player...';
 
-    let songUri = convertSpotifyUrlToUri(params.get('song-uri'));
-
-    class HirsterPlayer {
-
-      constructor(controller) {
-        let self = this;
-
-        this.spotifyController = controller;
-        this.spotifyController.addListener('ready', () => {
-          this.showPlayer();
-        });
-
-        this.play = document.getElementById('play-track');
-        this.player = document.getElementById('player');
-        this.message = document.getElementById('message');
-
-        this.play.addEventListener("click", function() {
-          self.playTrack();
-        });
-        this.pause.addEventListener("click", function() {
-          self.pauseTrack();
-        });
+      function convertSpotifyUrlToUri(url) {
+        return url.replace("https://open.spotify.com/", "spotify:").replace(/track\//, 'track:');
       }
+  
+      let songUri = convertSpotifyUrlToUri(params.get('song-uri'));
+  
+      class HirsterPlayer {
+  
+        constructor(controller) {
+          let self = this;
+  
+          this.spotifyController = controller;
+          this.spotifyController.addListener('ready', () => {
+            this.showPlayer();
+          });
+  
+          this.play = document.getElementById('play-track');
+          this.player = document.getElementById('player');
+          this.message = document.getElementById('message');
+  
+          this.play.addEventListener("click", function() {
+            self.playTrack();
+          });
+          this.pause.addEventListener("click", function() {
+            self.pauseTrack();
+          });
+        }
+  
+        showPlayer = function() {
+          this.player.style.display = 'block';
+          this.message.style.display = 'none';
+        }
+  
+        playTrack = function() {
+          if (this.spotifyController) {
+            this.spotifyController.togglePlay();
+          }
+        }
 
-      showPlayer = function() {
-        this.player.style.display = 'block';
-        this.message.style.display = 'none';
-      }
-
-      playTrack = function() {
-        if (this.spotifyController) {
-          this.spotifyController.togglePlay();
+        pauseTrack = function() {
+          if (this.spotifyController) {
+            this.spotifyController.pause();
+          }
         }
       }
+  
+      window.onSpotifyIframeApiReady = (IFrameAPI) => {
+        const element = document.getElementById('spotify-player');
+        const options = {
+        uri: songUri,
+      };	
+        if (params.has('secrets')) {	
+          document.getElementById('player-wrapper').style.visibility = 'visible';
+        }
+        else {
+          options.width = 0;
+          options.height = 0;	
+        }
+        IFrameAPI.createController(element, options, (controller) => {
+          let player = new HirsterPlayer(controller);
+        });
+      };
     }
-
-    window.onSpotifyIframeApiReady = (IFrameAPI) => {
-      const element = document.getElementById('spotify-player');
-      const options = {
-      uri: songUri,
-    };	
-      if (params.has('secrets')) {	
-        document.getElementById('player-wrapper').style.visibility = 'visible';
-      }
-      else {
-        options.width = 0;
-        options.height = 0;	
-      }
-      IFrameAPI.createController(element, options, (controller) => {
-        let player = new HirsterPlayer(controller);
-      });
-    };
+    else {
+      document.getElementById('message').textContent = 'Start by scanning QR-Code';
+    }
 
     // QR-Code reader.
     let html5QrCode = null; 
-    document.getElementById('scan-code').addEventListener("click", function() {
+    document.getElementById('scan-code').addEventListener('click', function() {
       let readerWrapper = document.getElementById('reader-wrapper');
       if (readerWrapper.style.display === 'none') {
+        if (typeof player === 'HirsterPlayer') {
+          player.pauseTrack();
+        }
         readerWrapper.style.display = 'block';
 
         html5QrCode = new Html5Qrcode("reader");
